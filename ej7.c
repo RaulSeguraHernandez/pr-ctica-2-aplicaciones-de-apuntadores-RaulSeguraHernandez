4f16f8c4597be8b467844de58febe8a1550ac660
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-03-02 17:53:04 -0600

    Create ej7.c

---
+++ ej7.c
@@ -0,0 +1,12 @@
+// 7. Accediendo a la Memoria
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    // Código para imprimir direcciones de memoria aquí
+    for (int i = 0; i <5; i++){
+        printf("%p \n", &arr[i]);
+    }
+    
+    return 0;
+}
