48e5c1cd1f1fd13f86f1ba0a0ebbefda2dc0e210
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-03-01 23:00:24 -0600

    Create e4.c

---
+++ e4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int *ptr = arr;
+  
+  printf("Valores originales:\n");
+  for (int i = 0; i < 5; i++) {
+    //Escribe el codigo para imprimir los valores usado el apuntador
+    printf("%d ", *(ptr+i));
+  }
+  printf("\n");
+
+  for (int i = 0; i < 5; i++) {
+    *(ptr+i) *= 2;
+  }
+  
+  printf("\nValores modificados:\n");
+  for (int i = 0; i < 5; i++) {
+    //Escribe el codigo para imprimir los valores usado el apuntador
+    printf("%d ", *(ptr+i));
+  }
+  printf("\n");
+  return 0;
+}
